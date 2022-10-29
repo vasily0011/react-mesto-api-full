9b79dc9daceee02bb80b209d859c10d40365a4b8
Vasily Boldyrev <vasil_001@mail.ru>
2022-10-29 14:37:57 +0300

    добавлено описание в README.md

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://domainname.backend.nomoredomains.icu";
+export const BASE_URL = "https://domainname.backend.nomoredomains.icu";
 
 function getResponse(res) {
   if (res.ok) {
